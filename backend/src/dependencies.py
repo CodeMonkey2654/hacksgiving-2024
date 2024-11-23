bc9a92edb2a672695cea0b314843bba92eed4389
Autumn Mizer <autumnmizer0624@gmail.com>
2024-11-22 20:24:41 -0600

    Resolves not existent merge conflicts

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:backend/src/dependencies.py
 from typing import Generator
 from database.database import SessionLocal
 
@@ -7,14 +6,4 @@ def get_db() -> Generator:
     try:
         yield db
     finally:
-=======
-from typing import Generator
-from database import SessionLocal
-
-def get_db() -> Generator:
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
->>>>>>> ecb15eb (ui amazing):backend/dependencies.py
         db.close() 
